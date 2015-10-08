61223bf73be8f543e771f0bd53a81802bbf6195f
Daniel <danielll20@hotmail.com>
2015-10-08 10:32:58 -0300

    Classe de acesso aos métodos de persistência.

---
+++ src/main/java/br/com/sistemaescolar/dao/AlunoDao.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.sistemaescolar.dao;
+
+
+/**
+ * Classe de acesso aos métodos de persistência.
+ * 
+ * @author Daniel Correia
+ * @since 31-08-2015
+ *
+ */
+public class AlunoDao extends GenericDao {
+	
+	
+}
