43a2ec6e15249689a1e0e7268b9f0ccb9098f0b0
Daniel <danielll20@hotmail.com>
2015-10-16 17:25:29 -0300

    Adicionado método buscarPorId e listar todos

--- src/main/java/br/com/sistemaescolar/dao/AlunoDao.java
+++ src/main/java/br/com/sistemaescolar/dao/AlunoDao.java
@@ -3,6 +3,13 @@
  */
 package br.com.sistemaescolar.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.sistemaescolar.modelo.Aluno;
 
 /**
  * Classe de acesso aos métodos de persistência.
@@ -12,6 +19,17 @@ package br.com.sistemaescolar.dao;
  *
  */
 public class AlunoDao extends GenericDao {
-	
-	
+
+	@Inject
+	private EntityManager entityManager;
+
+	public List<Aluno> listarTodos() {
+		Query query = entityManager.createQuery("select a from Aluno a ");
+
+		return query.getResultList();
+	}
+
+	public Aluno buscarPorId(Long id) {
+		return entityManager.find(Aluno.class, id);
+	}
 }
