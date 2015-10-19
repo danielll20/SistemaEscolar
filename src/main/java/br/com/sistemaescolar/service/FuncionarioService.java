34e5fb20b63157718e3ad9a505fa0f1fc7c47468
Leonardo <Leonardo@Leonardo-PC>
2015-10-19 12:59:50 -0300

    Criado a classe FuncinarioService

--- src/main/java/br/com/sistemaescolar/service/FuncionarioService.java
+++ src/main/java/br/com/sistemaescolar/service/FuncionarioService.java
@@ -9,7 +9,7 @@ import br.com.sistemaescolar.dao.FuncionarioDao;
 import br.com.sistemaescolar.modelo.Funcionario;
 
 /**
- @author Leonardo Ribeiro
+ @author Leonardo Ribeiro 
  * @since 19/10/2015
  */
 public class FuncionarioService {
