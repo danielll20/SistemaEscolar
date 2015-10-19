51758107c0902c3aced9890e19ae39da10798dea
Leonardo <Leonardo@Leonardo-PC>
2015-10-19 11:30:38 -0300

    Criado a classe FuncinarioService

---
+++ src/main/java/br/com/sistemaescolar/service/FuncionarioService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package br.com.sistemaescolar.service;
+
+import javax.inject.Inject;
+
+import br.com.sistemaescolar.dao.FuncionarioDao;
+import br.com.sistemaescolar.modelo.Funcionario;
+
+/**
+ @author Leonardo Ribeiro
+ * @since 19/10/2015
+ */
+public class FuncionarioService {
+	
+	@Inject
+	private FuncionarioDao funcionarioDao;
+	
+	public void insert(Funcionario funcionario) {
+		funcionarioDao.insert(funcionario);
+	}
+
+}
