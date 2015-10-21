2fa16975bdf587f8e64bd6e138d2160a399e5f2a
Daniel <danielll20@hotmail.com>
2015-10-20 22:28:30 -0200

    adicionado metodo listarInstituicaoPorNomeFantasia

--- src/main/java/br/com/sistemaescolar/dao/InstituicaoDao.java
+++ src/main/java/br/com/sistemaescolar/dao/InstituicaoDao.java
@@ -3,11 +3,28 @@
  */
 package br.com.sistemaescolar.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 /**
  * @author Leonardo Ribeiro
  *
  */
-public class InstituicaoDao extends GenericDao{
+public class InstituicaoDao extends GenericDao {
+	
+	@Inject
+	private EntityManager entityManager;
+
+	public String listarInstituicaoPorNomeFantasia() { 
+		Query query = entityManager.createQuery("select i.nomeFantasia from Instituicao i ");
+		
+		List<String> instituicao = query.getResultList();
+		
+		return instituicao.get(0);
+	}
 	
 
 
