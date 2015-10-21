2c51b719b0d0db4056d3ce44fe21792cc3e72aee
Leonardo <Leonardo@Leonardo-PC>
2015-10-21 13:09:28 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/dao/DisciplinaDao.java
+++ src/main/java/br/com/sistemaescolar/dao/DisciplinaDao.java
@@ -3,10 +3,31 @@
  */
 package br.com.sistemaescolar.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.sistemaescolar.modelo.Disciplina;
+
 /**
  * @author Leonardo Ribeiro
- *
+ * @since 21/10/2015
  */
 public class DisciplinaDao extends GenericDao {
+	
+	@Inject
+	private EntityManager entityManager;
+	
+	public List<Disciplina> listarTodos() {
+		Query query = entityManager.createQuery("select a from Disciplina a ");
+		
+		return query.getResultList();
+	}
+	
+	public Disciplina buscarPorId(Long id) {
+		return entityManager.find(Disciplina.class, id);
+	}
 
 }
