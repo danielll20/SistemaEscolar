c19555665aefd76e7d0302d0dec96c167d6cb1ff
Leonardo <Leonardo@Leonardo-PC>
2015-10-24 11:21:54 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/controller/FuncionarioController.java
+++ src/main/java/br/com/sistemaescolar/controller/FuncionarioController.java
@@ -3,36 +3,74 @@
  */
 package br.com.sistemaescolar.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.sistemaescolar.modelo.Funcionario;
 import br.com.sistemaescolar.service.FuncionarioService;
 
 /**
  * @author Leonardo Ribeiro
- * @since 19/10/2015
+ * @since 24/10/2015
  */
 @Controller
 public class FuncionarioController {
-	
+
 	@Inject
 	private FuncionarioService funcionarioService;
-	
+
 	@Inject
 	private Result result;
-	
+
+	@Inject
+	private Validator validator;
+
 	@Path("/funcionario/novo")
-    public void novo() {        
-    }
-	
+	public void novo() {
+	}
+
 	@Post
 	public void adiciona(Funcionario funcionario) {
 		funcionarioService.insert(funcionario);
 		result.redirectTo(FuncionarioController.class).novo();
+	}
+
+	@Get("/funcionario/listar")
+	public void listar() {
+		List<Funcionario> funcionarios = funcionarioService.listarTodos();
+		result.include("listaFuncionarios", funcionarios);
+	}
+
+	@Get("/funcionario/{funcionario.id}")
+	public void atualizarFormulario(Funcionario funcionario) {
+		result.include("funcionario", funcionarioService.buscarPorId(funcionario.getId()));
+	}
+
+	@Post("/funcionario/{id}")
+	@Transactional
+	public void atualizar(Long id, @Valid Funcionario funcionario) {
+		funcionario.setId(id);
+		validator.onErrorForwardTo(this).atualizarFormulario(funcionario);
+
+		funcionarioService.atualizar(funcionario);
+		result.redirectTo(this).listar();
+	}
+
+	@Get("/funcionario/delete/{id}")
+	@Transactional
+	public void remove(Long id) {
+		Funcionario funcionario = funcionarioService.buscarPorId(id);
+		funcionarioService.remove(funcionario);
+		result.redirectTo(this).listar();
 	}
 
 }
