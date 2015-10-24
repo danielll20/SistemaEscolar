fb8d9d927ceaa7c4ef518203894abd2d82a26614
Leonardo <Leonardo@Leonardo-PC>
2015-10-24 12:08:38 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/dao/InstituicaoDao.java
+++ src/main/java/br/com/sistemaescolar/dao/InstituicaoDao.java
@@ -3,6 +3,8 @@
  */
 package br.com.sistemaescolar.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -12,20 +14,28 @@ import br.com.sistemaescolar.modelo.Instituicao;
 
 /**
  * @author Leonardo Ribeiro
- *
+ * @since 24/10/2015
  */
 public class InstituicaoDao extends GenericDao {
-	
+
 	@Inject
 	private EntityManager entityManager;
 
-	public Instituicao listarInstituicaoPorNomeFantasia() throws NoResultException { 
-		Query query = entityManager.createQuery("select i from Instituicao i ");
+	public Instituicao listarInstituicaoPorNomeFantasia() throws NoResultException {
+		Query query = entityManager.createQuery("select i from Instituicao i");
 		query.setMaxResults(1);
-								
+
 		return (Instituicao) query.getSingleResult();
 	}
-	
 
+	public List<Instituicao> lstarTodos() {
+		Query query = entityManager.createQuery("select i from Instituicao i");
+
+		return query.getResultList();
+	}
+
+	public Instituicao buscarPorId(Long id) {
+		return entityManager.find(Instituicao.class, id);
+	}
 
 }
