f7c57f002c6ba580d74bdbc5e5f411bcdcf3e093
Leonardo <Leonardo@Leonardo-PC>
2015-10-24 14:00:07 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/dao/ProfessorDao.java
+++ src/main/java/br/com/sistemaescolar/dao/ProfessorDao.java
@@ -3,10 +3,30 @@
  */
 package br.com.sistemaescolar.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import br.com.sistemaescolar.modelo.Professor;
+
 /**
  * @author Leonardo Ribeiro
- * @since  15/10/2015
+ * @since  24/10/2015
  */
 public class ProfessorDao extends GenericDao {
+	
+	@Inject
+	private EntityManager entityManager;
+	
+	public List<Professor> listarTodos() {
+		Query query = entityManager.createQuery("select p from Professor p");
+
+		return query.getResultList();
+	}
+	
+	public Professor buscaPorId(Long id) {
+		return entityManager.find(Professor.class, id);
+	}
 
 }
