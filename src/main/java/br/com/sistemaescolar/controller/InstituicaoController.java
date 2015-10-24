1c73385c9b9953de3d6d6924a51443506e3c3534
Leonardo <Leonardo@Leonardo-PC>
2015-10-24 12:08:19 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/controller/InstituicaoController.java
+++ src/main/java/br/com/sistemaescolar/controller/InstituicaoController.java
@@ -3,18 +3,24 @@
  */
 package br.com.sistemaescolar.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.sistemaescolar.modelo.Instituicao;
 import br.com.sistemaescolar.service.InstituicaoService;
 
 /**
  * @author Leonardo Ribeiro
- * @since 10/10/2015
+ * @since 24/10/2015
  */
 @Controller
 public class InstituicaoController {
@@ -25,6 +31,9 @@ public class InstituicaoController {
 	@Inject 
 	private Result result;
 	
+	@Inject
+	private Validator validator;
+	
 	@Path("/instituicao/novo")
     public void novo() {        
     }
@@ -33,6 +42,35 @@ public class InstituicaoController {
 	public void adiciona(Instituicao instituicao) {
 		instituicaoService.insert(instituicao);
 		result.redirectTo(InstituicaoController.class).novo();
+	}
+	
+	@Get("/instituicao/listar")
+	public void listar() {
+		List<Instituicao> instituicoes = instituicaoService.listarTodos();
+		result.include("listaInstituicoes", instituicoes);
+	}
+	
+	@Get("/instituicao/{instituicao.id}")
+	public void atualizarFormulario(Instituicao instituicao) {
+		result.include("instituicao", instituicaoService.buscaPorId(instituicao.getId()));
+	}
+	
+	@Post("/instituicao/{id}")
+	@Transactional
+	public void atualizar(Long id, @Valid Instituicao instituicao) {
+		instituicao.setId(id);
+		validator.onErrorForwardTo(this).atualizarFormulario(instituicao);
+		
+		instituicaoService.atualizar(instituicao);
+		result.redirectTo(this).listar();
+	}
+	
+	@Get("/instituicao/delete/{id}")
+	@Transactional
+	public void remove(Long id) {
+		Instituicao instituicao = instituicaoService.buscaPorId(id);
+		instituicaoService.remove(instituicao);
+		result.redirectTo(this).listar();
 	}
 
 }
