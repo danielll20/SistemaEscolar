c0e35cccc9c930f80278d2348ccdc320492f899e
Leonardo <Leonardo@Leonardo-PC>
2015-10-24 11:22:45 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/service/FuncionarioService.java
+++ src/main/java/br/com/sistemaescolar/service/FuncionarioService.java
@@ -3,6 +3,8 @@
  */
 package br.com.sistemaescolar.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import br.com.sistemaescolar.dao.FuncionarioDao;
@@ -10,7 +12,7 @@ import br.com.sistemaescolar.modelo.Funcionario;
 
 /**
  @author Leonardo Ribeiro 
- * @since 19/10/2015
+ * @since 24/10/2015
  */
 public class FuncionarioService {
 	
@@ -19,6 +21,22 @@ public class FuncionarioService {
 	
 	public void insert(Funcionario funcionario) {
 		funcionarioDao.insert(funcionario);
+	}
+	
+	public List<Funcionario> listarTodos() {
+		return funcionarioDao.listarTodos();
+	}
+	
+	public Funcionario buscarPorId(Long id) {
+		return funcionarioDao.buscarPorId(id);
+	}
+	
+	public void remove(Funcionario funcionario) {
+		funcionarioDao.delete(funcionario);
+	}
+	
+	public void atualizar(Funcionario funcionario) {
+		funcionarioDao.update(funcionario);
 	}
 
 }
