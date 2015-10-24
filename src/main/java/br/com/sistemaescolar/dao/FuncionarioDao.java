36934445abfe78c8e91cd61e8a0d92bd96f847aa
Leonardo <Leonardo@Leonardo-PC>
2015-10-24 11:22:12 -0300

    Criado o CRUD completo.

--- src/main/java/br/com/sistemaescolar/dao/FuncionarioDao.java
+++ src/main/java/br/com/sistemaescolar/dao/FuncionarioDao.java
@@ -3,10 +3,31 @@
  */
 package br.com.sistemaescolar.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.sistemaescolar.modelo.Funcionario;
+
 /**
  * @author Leonardo Ribeiro 
- * @since 19/10/2015
+ * @since 24/10/2015
  */
 public class FuncionarioDao extends GenericDao {
+	
+	@Inject
+	private EntityManager entityManager;
+	
+	public List<Funcionario> listarTodos() {
+		Query query = entityManager.createQuery("select f from Funcionario f");
+		
+		return query.getResultList();
+	}
+	
+	public Funcionario buscarPorId(Long id) {
+		return entityManager.find(Funcionario.class, id);
+	}
 
 }
